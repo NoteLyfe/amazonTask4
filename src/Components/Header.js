a34186f5568f0d3b721b133a34dbd9a43e01ea4e
Ankesh Kumar <60851047+NoteLyfe@users.noreply.github.com>
2022-04-15 20:51:08 +0530

    bux fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,20 @@ import React from 'react'
 import logo from '../images/logo.png'
 
 const Header = () => {
+
+  setTimeout(() => {
+    let x = document.getElementById('pop');
+    let y = document.getElementById('btn-close');
+
+    x.addEventListener('click', function () {
+      document.getElementById("ham_nav").style.height = "200px"
+    })
+
+    y.addEventListener('click', function () {
+      document.getElementById("ham_nav").style.height = "0px"
+    })
+  }, 10);
+
   return (
     <>
       <header id="main-header">
@@ -13,6 +27,26 @@ const Header = () => {
               <li><a href="#catalog">Products</a></li>
             </ul>
           </div>
+          <div className="ham">
+            <span>
+                <a href="#" id='pop'>
+                    <svg width="30" height="30">
+                        <path d="M0,5 30,5" stroke="white" stroke-width="3" />
+                        <path d="M0,15 30,15" stroke="white" stroke-width="3" />
+                        <path d="M0,25 30,25" stroke="white" stroke-width="3" />
+                    </svg>
+                </a>
+            </span>
+        </div>
+
+        <div id="ham_nav" className="ham_item">
+            <button type="button" id="btn-close">&times;</button>
+            <ul className="side_nav">
+                <li><a href="/">Home</a></li>
+                <li><a href=" ">Featured</a></li>
+                <li><a href="#catalog">Products</a></li>
+            </ul>
+        </div>
       </header>
     </>
   )
